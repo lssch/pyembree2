4022115b49b21643810328d787fdc1d9fef3c6ff
Lars Schwarz <info@larsschwarz.ch>
2026-09-03 13:38:52 +0200

    fixed prebuilt embree fetching on mac

--- cmake/fetch-embree.cmake
+++ cmake/fetch-embree.cmake
@@ -15,82 +15,28 @@ else()
 endif()
 
 if(EMBREE_BUILD_FROM_SOURCE)
-    message(STATUS
-        "No prebuilt Embree available for "
-        "${CMAKE_SYSTEM_NAME}/${CMAKE_SYSTEM_PROCESSOR}; "
-        "building from source"
-    )
-
+    message(STATUS "No prebuilt Embree available; building from source")
     set(EMBREE_TASKING_SYSTEM "INTERNAL" CACHE STRING "" FORCE)
     set(EMBREE_ISPC_SUPPORT OFF CACHE BOOL "" FORCE)
     set(EMBREE_TUTORIALS OFF CACHE BOOL "" FORCE)
-
     FetchContent_Declare(
         embree
         GIT_REPOSITORY https://github.com/RenderKit/embree.git
         GIT_TAG "v${EMBREE_VERSION}"
     )
-
     FetchContent_MakeAvailable(embree)
 else()
     set(EMBREE_URL
         "https://github.com/RenderKit/embree/releases/download/v${EMBREE_VERSION}/${EMBREE_ARCHIVE}"
     )
-
-    message(STATUS
-        "Prebuilt Embree available for "
-        "${CMAKE_SYSTEM_NAME}/${CMAKE_SYSTEM_PROCESSOR}; "
-        "downloading ${EMBREE_URL}"
+    message(STATUS "Downloading prebuilt Embree from ${EMBREE_URL}")
+    FetchContent_Declare(
+        embree
+        URL "${EMBREE_URL}"
     )
-
-    set(EMBREE_ROOT
-        "${CMAKE_BINARY_DIR}/_embree"
+    FetchContent_MakeAvailable(embree)
+    find_package(embree ${EMBREE_VERSION} EXACT CONFIG REQUIRED
+        PATHS "${embree_SOURCE_DIR}"
+        NO_DEFAULT_PATH
     )
-
-    file(REMOVE_RECURSE "${EMBREE_ROOT}")
-    file(MAKE_DIRECTORY "${EMBREE_ROOT}")
-
-    set(EMBREE_ARCHIVE_PATH
-        "${CMAKE_BINARY_DIR}/${EMBREE_ARCHIVE}"
-    )
-
-    file(DOWNLOAD
-        "${EMBREE_URL}"
-        "${EMBREE_ARCHIVE_PATH}"
-        SHOW_PROGRESS
-        TLS_VERIFY ON
-    )
-
-    file(ARCHIVE_EXTRACT
-        INPUT "${EMBREE_ARCHIVE_PATH}"
-        DESTINATION "${EMBREE_ROOT}"
-    )
-
-    file(GLOB EMBREE_DIRS
-        "${EMBREE_ROOT}/*"
-    )
-    if(NOT EXISTS "${EMBREE_ROOT}/include")
-        list(GET EMBREE_DIRS 0 EMBREE_ROOT)
-    endif()
-
-    if(WIN32)
-        add_library(embree UNKNOWN IMPORTED GLOBAL)
-        set_target_properties(embree PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${EMBREE_ROOT}/include"
-            IMPORTED_IMPLIB "${EMBREE_ROOT}/lib/embree4.lib"
-            IMPORTED_LOCATION "${EMBREE_ROOT}/bin/embree4.dll"
-        )
-    elseif(APPLE)
-        add_library(embree SHARED IMPORTED GLOBAL)
-        set_target_properties(embree PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${EMBREE_ROOT}/include"
-            IMPORTED_LOCATION "${EMBREE_ROOT}/lib/libembree4.dylib"
-        )
-    else()
-        add_library(embree SHARED IMPORTED GLOBAL)
-        set_target_properties(embree PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${EMBREE_ROOT}/include"
-            IMPORTED_LOCATION "${EMBREE_ROOT}/lib/libembree4.so"
-        )
-    endif()
 endif()
