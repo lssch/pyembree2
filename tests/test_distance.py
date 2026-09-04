245cce1c0e556493f6a347e312d5d1925a6ad1d3
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2026-09-04 09:36:19 +0200

    Adds testing for signed distances

---
+++ tests/test_distance.py
@@ -0,0 +1,68 @@
+import numpy as np
+import trimesh
+
+import pyembree2
+
+
+def test_distance_multiple_meshes(random_generator):
+    trimesh_0_mesh = trimesh.creation.random_soup(100, seed=random_generator)
+    trimesh_1_mesh = trimesh.creation.random_soup(300, seed=random_generator)
+
+    points = random_generator.uniform(-0.5, 0.5, (200, 3))
+
+    trimesh_0_closest_points, trimesh_0_distances, trimesh_0_triangle_id = (
+        trimesh_0_mesh.nearest.on_surface(points)
+    )
+    trimesh_1_closest_points, trimesh_1_distances, trimesh_1_triangle_id = (
+        trimesh_1_mesh.nearest.on_surface(points)
+    )
+
+    trimesh_mesh_index = np.argmin(
+        np.stack([trimesh_0_distances, trimesh_1_distances], axis=0), axis=0
+    )
+
+    trimesh_closest_points = np.empty_like(trimesh_0_closest_points)
+    trimesh_closest_points[trimesh_mesh_index == 0] = trimesh_0_closest_points[
+        trimesh_mesh_index == 0
+    ]
+    trimesh_closest_points[trimesh_mesh_index == 1] = trimesh_1_closest_points[
+        trimesh_mesh_index == 1
+    ]
+
+    trimesh_distances = np.empty_like(trimesh_0_distances)
+    trimesh_distances[trimesh_mesh_index == 0] = trimesh_0_distances[
+        trimesh_mesh_index == 0
+    ]
+    trimesh_distances[trimesh_mesh_index == 1] = trimesh_1_distances[
+        trimesh_mesh_index == 1
+    ]
+
+    trimesh_triangle_id = np.empty_like(trimesh_0_distances)
+    trimesh_triangle_id[trimesh_mesh_index == 0] = trimesh_0_triangle_id[
+        trimesh_mesh_index == 0
+    ]
+    trimesh_triangle_id[trimesh_mesh_index == 1] = trimesh_1_triangle_id[
+        trimesh_mesh_index == 1
+    ]
+
+    pyembree_0_mesh = pyembree2.triangle_mesh(
+        vertices=trimesh_0_mesh.vertices, faces=trimesh_0_mesh.faces
+    )
+    pyembree_1_mesh = pyembree2.triangle_mesh(
+        vertices=trimesh_1_mesh.vertices, faces=trimesh_1_mesh.faces
+    )
+    (
+        pyembree_mesh_index,
+        pyembree_triangle_id,
+        pyembree_closest_points,
+        pyembree_singed_distances,
+    ) = pyembree2.distance(meshes=[pyembree_0_mesh, pyembree_1_mesh], points=points)
+
+    np.testing.assert_equal(trimesh_mesh_index, pyembree_mesh_index)
+    np.testing.assert_equal(trimesh_triangle_id, pyembree_triangle_id)
+    np.testing.assert_allclose(
+        trimesh_closest_points, pyembree_closest_points, atol=1e-6
+    )
+    np.testing.assert_allclose(
+        trimesh_distances, np.abs(pyembree_singed_distances), atol=1e-6
+    )
--- tests/test_singed_distance.py
+++ tests/test_singed_distance.py
@@ -1,68 +1,34 @@
 import numpy as np
 import trimesh
 
-import pyembree2
 
+def test_signed_distance_spheres(random_generator):
+    sphere = trimesh.creation.icosphere(radius=3.0)
+    sphere.apply_translation((4.0, -2.0, 1.0))
+    sphere.visual.face_colors = [0, 255, 0, 255]
+    cone = trimesh.creation.cone(radius=4.0, height=6.0)
+    cone.apply_translation((-3.0, 1.0, 4.0))
+    cone.visual.face_colors = [0, 0, 255, 255]
+    box = trimesh.creation.box(extents=[5, 3, 7])
+    box.apply_translation((2.0, 3.0, -4.0))
+    box.visual.face_colors = [255, 255, 0, 255]
+    scene = [sphere, cone, box]
+    bounds = trimesh.Scene([sphere, cone, box]).bounds
 
-def test_distance_multiple_meshes(random_generator):
-    trimesh_0_mesh = trimesh.creation.random_soup(100, seed=random_generator)
-    trimesh_1_mesh = trimesh.creation.random_soup(300, seed=random_generator)
+    n = 200
+    points = np.zeros((n, 3))
+    points[:, 0] = random_generator.uniform(*bounds[:, 0], n)
+    points[:, 1] = random_generator.uniform(*bounds[:, 1], n)
+    points[:, 2] = random_generator.uniform(*bounds[:, 2], n)
 
-    points = random_generator.uniform(-0.5, 0.5, (200, 3))
+    if False:
+        pcl = trimesh.points.PointCloud(points, colors=[255, 0, 0, 255])
+        scene = trimesh.Scene(scene + [pcl])
+        scene.show()
 
-    trimesh_0_closest_points, trimesh_0_distances, trimesh_0_triangle_id = (
-        trimesh_0_mesh.nearest.on_surface(points)
-    )
-    trimesh_1_closest_points, trimesh_1_distances, trimesh_1_triangle_id = (
-        trimesh_1_mesh.nearest.on_surface(points)
-    )
+    signed_distances = np.zeros((n, len(scene)))
+    for i, mesh in enumerate(scene):
+        signed_distances[:, i] = trimesh.proximity.signed_distance(mesh, points)
 
-    trimesh_mesh_index = np.argmin(
-        np.stack([trimesh_0_distances, trimesh_1_distances], axis=0), axis=0
-    )
-
-    trimesh_closest_points = np.empty_like(trimesh_0_closest_points)
-    trimesh_closest_points[trimesh_mesh_index == 0] = trimesh_0_closest_points[
-        trimesh_mesh_index == 0
-    ]
-    trimesh_closest_points[trimesh_mesh_index == 1] = trimesh_1_closest_points[
-        trimesh_mesh_index == 1
-    ]
-
-    trimesh_distances = np.empty_like(trimesh_0_distances)
-    trimesh_distances[trimesh_mesh_index == 0] = trimesh_0_distances[
-        trimesh_mesh_index == 0
-    ]
-    trimesh_distances[trimesh_mesh_index == 1] = trimesh_1_distances[
-        trimesh_mesh_index == 1
-    ]
-
-    trimesh_triangle_id = np.empty_like(trimesh_0_distances)
-    trimesh_triangle_id[trimesh_mesh_index == 0] = trimesh_0_triangle_id[
-        trimesh_mesh_index == 0
-    ]
-    trimesh_triangle_id[trimesh_mesh_index == 1] = trimesh_1_triangle_id[
-        trimesh_mesh_index == 1
-    ]
-
-    pyembree_0_mesh = pyembree2.triangle_mesh(
-        vertices=trimesh_0_mesh.vertices, faces=trimesh_0_mesh.faces
-    )
-    pyembree_1_mesh = pyembree2.triangle_mesh(
-        vertices=trimesh_1_mesh.vertices, faces=trimesh_1_mesh.faces
-    )
-    (
-        pyembree_mesh_index,
-        pyembree_triangle_id,
-        pyembree_closest_points,
-        pyembree_singed_distances,
-    ) = pyembree2.distance(meshes=[pyembree_0_mesh, pyembree_1_mesh], points=points)
-
-    np.testing.assert_equal(trimesh_mesh_index, pyembree_mesh_index)
-    np.testing.assert_equal(trimesh_triangle_id, pyembree_triangle_id)
-    np.testing.assert_allclose(
-        trimesh_closest_points, pyembree_closest_points, atol=1e-6
-    )
-    np.testing.assert_allclose(
-        trimesh_distances, np.abs(pyembree_singed_distances), atol=1e-6
-    )
+    mesh_indices = np.argmin(np.abs(signed_distances), axis=1)
+    signed_distances = signed_distances[np.arange(n), mesh_indices]
